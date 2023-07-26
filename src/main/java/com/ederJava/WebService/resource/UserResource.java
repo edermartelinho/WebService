a2b36e69d5a44174b923e107e5e44b80d9787620
eder <edermartelinho@hotmail.com>
2023-07-26 17:00:32 -0300

    Create User e resource

---
+++ src/main/java/com/ederJava/WebService/resource/UserResource.java
@@ -0,0 +1,19 @@
+package com.ederJava.WebService.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ederJava.WebService.entity.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","Maria@mnnc.com","9999999","123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
