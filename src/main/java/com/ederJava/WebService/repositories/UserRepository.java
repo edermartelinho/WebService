da85543d7cab7c01d6b0004b29b2fb13545235d9
eder <edermartelinho@hotmail.com>
2023-07-26 21:36:35 -0300

    DB seeding, @Autowired, JpaRepository

---
+++ src/main/java/com/ederJava/WebService/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ederJava.WebService.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ederJava.WebService.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
