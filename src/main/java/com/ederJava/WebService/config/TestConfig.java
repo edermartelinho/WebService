bb8c616523127a4bedb13424e4bbc6399759d422
eder <edermartelinho@hotmail.com>
2023-08-02 19:50:48 -0300

    Association With Payment

--- src/main/java/com/ederJava/WebService/config/TestConfig.java
+++ src/main/java/com/ederJava/WebService/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ederJava.WebService.entity.Category;
 import com.ederJava.WebService.entity.Order;
 import com.ederJava.WebService.entity.OrderItem;
+import com.ederJava.WebService.entity.Payment;
 import com.ederJava.WebService.entity.Product;
 import com.ederJava.WebService.entity.User;
 import com.ederJava.WebService.entity.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2023-06-20T21:53:07Z") ,o1);
+		
+		o1.setPayment(pay1);
+	
+		orderRepository.save(o1);
 	}
 
 }
