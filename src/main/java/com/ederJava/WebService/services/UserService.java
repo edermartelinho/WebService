0002285c88c4ec8656f57e260683e8fb7495a669
eder <edermartelinho@hotmail.com>
2023-08-04 20:43:07 -0300

    treatment Exception handling, findById

--- src/main/java/com/ederJava/WebService/services/UserService.java
+++ src/main/java/com/ederJava/WebService/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ederJava.WebService.entity.User;
 import com.ederJava.WebService.repositories.UserRepository;
+import com.ederJava.WebService.services.exception.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
